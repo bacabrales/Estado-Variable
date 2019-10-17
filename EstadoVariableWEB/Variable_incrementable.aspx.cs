08837bb65b1ae68deef206b269eb1cda26d54398
bacabrales <bacabrales@misena.edu.co>
2019-10-17 17:05:49 -0500

    Actualizar proyecto

---
+++ EstadoVariableWEB/Variable_incrementable.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EstadoVariableWEB
+{
+    
+    public partial class Variable_incrementable : System.Web.UI.Page
+    {
+        private int numero;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+            if (!IsPostBack)
+            {
+                txtNumero.Text = "0";
+            }
+        }
+
+        protected void btnIncrementar_Click(object sender, EventArgs e)
+        {
+            if(ViewState["conteo"] != null)
+            {
+                numero = (int)ViewState["conteo"] + 1;
+                
+            }
+            txtNumero.Text = numero.ToString();
+            ViewState["conteo"] = numero;
+        }
+    }
+}
---
+++ EstadoVariableWEB/Variable_incrementable.aspx.designer.cs
@@ -0,0 +1,42 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace EstadoVariableWEB {
+    
+    
+    public partial class Variable_incrementable {
+        
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// Control txtNumero.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtNumero;
+        
+        /// <summary>
+        /// Control btnIncrementar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnIncrementar;
+    }
+}
